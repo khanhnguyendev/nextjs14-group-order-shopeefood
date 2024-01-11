44d48252eb9b5a24a08ddaa40a59b6e0777d9212
nguyen.nk <khanhnguyen.vlu@gmail.com>
2024-01-11 16:51:16 +0700

    #UI homepage + hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grip grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Order ShopeeFood: Together as a Group!</h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora
+              officia porro enim eaque magni nostrum inventore totam suscipit,
+              minus quaerat quibusdam rem at dolor quos iure placeat excepturi,
+              expedita aperiam?
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#rooms">Create Room</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="rooms"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">History Room</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Seach CategoryFilter
+        </div>
+      </section>
+    </>
+  );
 }
