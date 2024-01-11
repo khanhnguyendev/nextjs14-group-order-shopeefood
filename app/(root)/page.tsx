5c2bf7c412d1b51309e8935caf7d1f4d76c9752c
nguyen.nk <khanhnguyen.vlu@gmail.com>
2024-01-11 16:44:03 +0700

    #UI header & footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-4xl">Group Order</h1>
-    </main>
-  );
+  return <main></main>;
 }
