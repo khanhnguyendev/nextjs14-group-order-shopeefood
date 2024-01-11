86f45ccff2af50bab0e990e1b9a664f23f7bdf6e
nguyen.nk <khanhnguyen.vlu@gmail.com>
2024-01-11 15:46:38 +0700

    Initial Commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col ">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Group Order</h1>
+    </main>
+  );
+}
